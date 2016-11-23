c33b55d03a74a4f18243557aecf26ce52e168c9f
Maru <marianotrebino@gmail.com>
2016-11-23 12:09:23 -0500

    Fixed allocate with alignment.

--- src/LinearAllocator.cpp
+++ src/LinearAllocator.cpp
@@ -12,10 +12,8 @@ LinearAllocator::~LinearAllocator(){
 	// Do nothing - Parent frees memory for us
 }
 
-void* LinearAllocator::Allocate(const std::size_t size){
+void* LinearAllocator::Allocate(const std::size_t size, const std::size_t alignment){
 	const long currentAddress = (long)m_start_ptr + m_offset;
-	//const std::size_t alignment = this->calculateAlignment(currentAddress, size);
-	const std::size_t alignment = size;
 	uint32_t displacement = 0;
 	if (m_offset % alignment != 0) {
 		// Alignment is required. Find the next aligned memory address and update offset
@@ -26,13 +24,13 @@ void* LinearAllocator::Allocate(const std::size_t size){
 	// Offset is pointing to an aligned memory address
 	const long nextAddress = (long) m_start_ptr + m_offset;
 	m_offset += size;
-	std::cout << "Adress\t\t" << (void*)nextAddress << "\tSize\t" << size << "\tOffset\t" << m_offset << "\tDisplacement\t" << displacement << std::endl;
+	std::cout << "Adress\t\t" << (void*)nextAddress << "\tSize\t" << size << "\tOffset\t" << m_offset << "\tDisplacement\t" << displacement << "\tAlignment\t" << alignment<< std::endl;
 
 	return (void*) nextAddress;
 }
 
 void LinearAllocator::Free(void* ptr) {
-	assert// Do nothing - A linear allocator does not frees individual memory blocks
+	assert(ptr==ptr);// Do nothing - A linear allocator does not frees individual memory blocks
 }
 
 void LinearAllocator::Reset() {
