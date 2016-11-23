e5b6979d9e1d8d5ac44eefed24521793d3ebd43f
Maru <marianotrebino@gmail.com>
2016-11-23 12:46:11 -0500

    Added checker to avoid x % 0 operations.

--- src/LinearAllocator.cpp
+++ src/LinearAllocator.cpp
@@ -15,7 +15,7 @@ LinearAllocator::~LinearAllocator(){
 void* LinearAllocator::Allocate(const std::size_t size, const std::size_t alignment){
 	const long currentAddress = (long)m_start_ptr + m_offset;
 	uint32_t displacement = 0;
-	if (m_offset % alignment != 0) {
+	if (alignment!= 0 && m_offset % alignment != 0) {
 		// Alignment is required. Find the next aligned memory address and update offset
 		const long multiplier = (m_offset / alignment) + 1;
 		displacement = (multiplier * alignment) - m_offset;
@@ -24,8 +24,9 @@ void* LinearAllocator::Allocate(const std::size_t size, const std::size_t alignm
 	// Offset is pointing to an aligned memory address
 	const long nextAddress = (long) m_start_ptr + m_offset;
 	m_offset += size;
+
 #ifdef 	DEBUG
-	std::cout << "\tAdress\t\t" << (void*)nextAddress << "\tSize\t" << size << "\tOffset\t" << m_offset << "\tDisplacement\t" << displacement << "\tAlignment\t" << alignment<< std::endl;
+	std::cout << "\t\tAdress\t\t" << (void*)nextAddress << "\tSize\t" << size << "\tOffset\t" << m_offset << "\tDisplacement\t" << displacement << "\tAlignment\t" << alignment<< std::endl;
 #endif
 
 	return (void*) nextAddress;
