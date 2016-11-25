2a339c717a587dfec9041c5dcc27711ae86c89d9
Maru <marianotrebino@gmail.com>
2016-11-25 11:27:47 -0500

    Added freeing benchmark

--- src/BenchmarkStack.cpp
+++ src/BenchmarkStack.cpp
@@ -29,8 +29,35 @@ BenchmarkResults BenchmarkStack::allocation() {
 
 
 BenchmarkResults BenchmarkStack::freeing() {
-	return buildResults(0, 0, 0, 0);
-}
+	setStartTimer();
+	
+	StackAllocator stackAllocator(1e10);
+
+	std::size_t operations = 0;
+	double elapsedTime = 0;
+	while(elapsedTime < (m_runtime * 1e3)){
+		if (operations % 2 == 0){
+			stackAllocator.Allocate(sizeof(int), alignof(int));			// 4  -> 4
+			stackAllocator.Allocate(sizeof(bool), alignof(bool));		// 1  -> 5
+																	// 3  -> 8
+			stackAllocator.Allocate(sizeof(foo), alignof(foo));			// 16 -> 24
+		}else {
+			timespec before_free, after_free;
+			setTimer(before_free);
+			stackAllocator.Free(sizeof(foo));
+			stackAllocator.Free(sizeof(bool));
+			stackAllocator.Free(sizeof(int));
+			setTimer(after_free);
+			elapsedTime += calculateElapsedTime(before_free, after_free);
+		}
+		++operations;
+	}
+
+	BenchmarkResults results = buildResults(operations/2, m_runtime, 0, 0);
+	
+	printResults(results);
+	stackAllocator.Reset();
+	return results;}
 
 BenchmarkResults BenchmarkStack::read() {
 	return buildResults(0, 0, 0, 0);
