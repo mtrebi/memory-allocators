764632646a762e3ce9269676bf982b033d5d7785
Maru <marianotrebino@gmail.com>
2016-11-27 16:48:32 -0500

    Rename first parameter to baseAddres to avoid confusion

--- includes/Utils.h
+++ includes/Utils.h
@@ -3,12 +3,12 @@
 
 class Utils {
 public:
-	static const std::size_t CalculatePadding(const std::size_t offset, const std::size_t alignment) {
-		const std::size_t multiplier = (offset / alignment) + 1;
-		const std::size_t padding = (multiplier * alignment) - offset;
+	static const std::size_t CalculatePadding(const std::size_t baseAddress, const std::size_t alignment) {
+		const std::size_t multiplier = (baseAddress / alignment) + 1;
+		const std::size_t alignedAddress = multiplier * alignment;
+		const std::size_t padding = alignedAddress - baseAddress;
 		return padding;
 	}
-
 };
 
 #endif /* UTILS_H */
