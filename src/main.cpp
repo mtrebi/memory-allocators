2cb2769aca604f4eb89bd858ee12f1bed306205b
Maru <marianotrebino@gmail.com>
2016-11-28 15:51:09 -0500

    Added pool to benchmark

--- src/main.cpp
+++ src/main.cpp
@@ -10,54 +10,42 @@
 #include "PoolAllocator.h"
 
 int main(){
-	/*
+	
 	const std::vector<std::size_t> ALLOCATION_SIZES {2, 4, 16, 32, 64, 256, 512, 1024};
 	const std::vector<std::size_t> ALIGNMENTS {2, 4, 8, 8, 8, 8, 8, 8};
 
 	Allocator * cAllocator = new CAllocator();
 	Allocator * linearAllocator = new LinearAllocator(1e8);
 	Allocator * stackAllocator = new StackAllocator(1e8);
-	Allocator * poolAllocator = new PoolAllocator(32, 4);
+	Allocator * poolAllocator = new PoolAllocator(4096, 8);
 
-	Benchmark benchmark(100, ALLOCATION_SIZES, ALIGNMENTS);
+	Benchmark benchmark(100);
 	
 	std::cout << "C" << std::endl;
-	benchmark.Allocation(cAllocator);
-	benchmark.Free(cAllocator);
-	benchmark.AllocationRandom(cAllocator);
-	benchmark.FreeRandom(cAllocator);
+	benchmark.MultipleAllocation(cAllocator, ALLOCATION_SIZES, ALIGNMENTS);
+	benchmark.MultipleFree(cAllocator, ALLOCATION_SIZES, ALIGNMENTS);
+	benchmark.RandomAllocation(cAllocator, ALLOCATION_SIZES, ALIGNMENTS);
+	benchmark.RandomFree(cAllocator, ALLOCATION_SIZES, ALIGNMENTS);
 
 	std::cout << "LINEAR" << std::endl;
-	benchmark.Allocation(linearAllocator);
-	benchmark.AllocationRandom(linearAllocator);
+	benchmark.MultipleAllocation(linearAllocator, ALLOCATION_SIZES, ALIGNMENTS);
+	benchmark.RandomAllocation(linearAllocator, ALLOCATION_SIZES, ALIGNMENTS);
 
 	std::cout << "STACK" << std::endl;
-	benchmark.Allocation(stackAllocator);
-	benchmark.Free(stackAllocator);
-	benchmark.AllocationRandom(stackAllocator);
-	benchmark.FreeRandom(stackAllocator);
+	benchmark.MultipleAllocation(stackAllocator, ALLOCATION_SIZES, ALIGNMENTS);
+	benchmark.MultipleFree(stackAllocator, ALLOCATION_SIZES, ALIGNMENTS);
+	benchmark.RandomAllocation(stackAllocator, ALLOCATION_SIZES, ALIGNMENTS);
+	benchmark.RandomFree(stackAllocator, ALLOCATION_SIZES, ALIGNMENTS);
 
-	//std::cout << "POOL" << std::endl;
-	//benchmark.Allocation(poolAllocator); 
-	//benchmark.Free(poolAllocator);
+	std::cout << "POOL" << std::endl;
+	benchmark.SingleAllocation(poolAllocator, 8, 0);
+	benchmark.SingleFree(poolAllocator, 8, 0);
 
 	delete cAllocator;
 	delete linearAllocator;
 	delete stackAllocator;
 	delete poolAllocator;
-	*/
-
-	Allocator * poolAllocator = new PoolAllocator(32, 4);
-
-	poolAllocator->Init();
-	void * a = poolAllocator->Allocate(4, 4);
-	void * b = poolAllocator->Allocate(4, 4);
-	void * c = poolAllocator->Allocate(4, 4);
-	poolAllocator->Free(b);
-	poolAllocator->Free(c);
-	poolAllocator->Free(a);
-	void * d = poolAllocator->Allocate(4, 4);
-
+	
 	return 1;
 }
 
