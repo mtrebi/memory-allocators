4f5b751142749ab2780c4de95485e3413c5ebfd9
maru <marianotrebino@gmail.com>
2016-12-02 17:02:37 -0500

    Added Free list allocator benchmark

--- src/main.cpp
+++ src/main.cpp
@@ -8,45 +8,54 @@
 #include "CAllocator.h"
 #include "LinearAllocator.h"
 #include "PoolAllocator.h"
+#include "FreeListAllocator.h"
 
-int main(){
-	
-	const std::vector<std::size_t> ALLOCATION_SIZES {2, 4, 16, 32, 64, 256, 512, 1024};
-	const std::vector<std::size_t> ALIGNMENTS {2, 4, 8, 8, 8, 8, 8, 8};
+int main() {
+    const std::vector<std::size_t> ALLOCATION_SIZES {32, 64, 256, 512, 1024};
+    const std::vector<std::size_t> ALIGNMENTS {2, 4, 8, 8, 8};
+    //const std::vector<std::size_t> ALLOCATION_SIZES {2, 4, 16, 32, 64, 256, 512, 1024};
+    //const std::vector<std::size_t> ALIGNMENTS {2, 4, 8, 8, 8, 8, 8, 8};
 
-	Allocator * cAllocator = new CAllocator();
-	Allocator * linearAllocator = new LinearAllocator(1e8);
-	Allocator * stackAllocator = new StackAllocator(1e8);
-	Allocator * poolAllocator = new PoolAllocator(4096, 8);
+    Allocator * cAllocator = new CAllocator();
+    Allocator * linearAllocator = new LinearAllocator(1e8);
+    Allocator * stackAllocator = new StackAllocator(1e8);
+    //Allocator * poolAllocator = new PoolAllocator(4096, 8);
+    Allocator * freeListAllocator = new FreeListAllocator(1e8, FreeListAllocator::PlacementPolicy::FIND_FIRST, FreeListAllocator::StoragePolicy::SORTED);
 
-	Benchmark benchmark(100);
-	
-	std::cout << "C" << std::endl;
-	benchmark.MultipleAllocation(cAllocator, ALLOCATION_SIZES, ALIGNMENTS);
-	benchmark.MultipleFree(cAllocator, ALLOCATION_SIZES, ALIGNMENTS);
-	benchmark.RandomAllocation(cAllocator, ALLOCATION_SIZES, ALIGNMENTS);
-	benchmark.RandomFree(cAllocator, ALLOCATION_SIZES, ALIGNMENTS);
+    Benchmark benchmark(100);
+    
+    std::cout << "C" << std::endl;
+    benchmark.MultipleAllocation(cAllocator, ALLOCATION_SIZES, ALIGNMENTS);
+    benchmark.MultipleFree(cAllocator, ALLOCATION_SIZES, ALIGNMENTS);
+    benchmark.RandomAllocation(cAllocator, ALLOCATION_SIZES, ALIGNMENTS);
+    benchmark.RandomFree(cAllocator, ALLOCATION_SIZES, ALIGNMENTS);
 
-	std::cout << "LINEAR" << std::endl;
-	benchmark.MultipleAllocation(linearAllocator, ALLOCATION_SIZES, ALIGNMENTS);
-	benchmark.RandomAllocation(linearAllocator, ALLOCATION_SIZES, ALIGNMENTS);
+    std::cout << "LINEAR" << std::endl;
+    benchmark.MultipleAllocation(linearAllocator, ALLOCATION_SIZES, ALIGNMENTS);
+    benchmark.RandomAllocation(linearAllocator, ALLOCATION_SIZES, ALIGNMENTS);
 
-	std::cout << "STACK" << std::endl;
-	benchmark.MultipleAllocation(stackAllocator, ALLOCATION_SIZES, ALIGNMENTS);
-	benchmark.MultipleFree(stackAllocator, ALLOCATION_SIZES, ALIGNMENTS);
-	benchmark.RandomAllocation(stackAllocator, ALLOCATION_SIZES, ALIGNMENTS);
-	benchmark.RandomFree(stackAllocator, ALLOCATION_SIZES, ALIGNMENTS);
+    std::cout << "STACK" << std::endl;
+    benchmark.MultipleAllocation(stackAllocator, ALLOCATION_SIZES, ALIGNMENTS);
+    benchmark.MultipleFree(stackAllocator, ALLOCATION_SIZES, ALIGNMENTS);
+    benchmark.RandomAllocation(stackAllocator, ALLOCATION_SIZES, ALIGNMENTS);
+    benchmark.RandomFree(stackAllocator, ALLOCATION_SIZES, ALIGNMENTS);
 
-	std::cout << "POOL" << std::endl;
-	benchmark.SingleAllocation(poolAllocator, 8, 0);
-	benchmark.SingleFree(poolAllocator, 8, 0);
+    std::cout << "POOL" << std::endl;
+    benchmark.SingleAllocation(poolAllocator, 8, 0);
+    benchmark.SingleFree(poolAllocator, 8, 0);
 
-	delete cAllocator;
-	delete linearAllocator;
-	delete stackAllocator;
-	delete poolAllocator;
-	
-	return 1;
+    std::cout << "FREE LIST" << std::endl;
+    benchmark.MultipleAllocation(freeListAllocator, ALLOCATION_SIZES, ALIGNMENTS);
+    benchmark.MultipleFree(freeListAllocator, ALLOCATION_SIZES, ALIGNMENTS);
+    benchmark.RandomAllocation(freeListAllocator, ALLOCATION_SIZES, ALIGNMENTS);
+    benchmark.RandomFree(freeListAllocator, ALLOCATION_SIZES, ALIGNMENTS);
+    
+    delete cAllocator;
+    delete linearAllocator;
+    delete stackAllocator;
+    //delete poolAllocator;
+    
+    return 1;
 }
 
 
