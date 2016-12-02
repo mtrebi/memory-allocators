9c130733fa3dbc5826a423f14af160ce0e0c65c5
maru <marianotrebino@gmail.com>
2016-12-02 10:32:01 -0500

    Added edge case when we remove the first node or the last node

--- includes/DoublyLinkedListImpl.h
+++ includes/DoublyLinkedListImpl.h
@@ -1,38 +1,48 @@
 #include "DoublyLinkedList.h"
 
 template <class T>
-DoublyLinkedList<T>::DoublyLinkedList(){
-	
+DoublyLinkedList<T>::DoublyLinkedList() {
+
 }
 
 template <class T>
-void DoublyLinkedList<T>::insert(Node<T>* previousNode, Node<T>* newNode){
-	if (previousNode == nullptr){
-		// Should be inserted at the beginning
-		if (head != nullptr){
-			// There are more nodes
-			newNode->next = head->next;
-			newNode->next->previous = newNode;
-		}
-		head = newNode;
-		newNode->previous = nullptr;
-	}
-	else if (previousNode->next == nullptr) {
-		// Should be inserted at the end
-		previousNode->next = newNode;
-		newNode->previous = previousNode;
-		newNode->next = nullptr;
-	}
+void DoublyLinkedList<T>::insert(Node<T>* previousNode, Node<T>* newNode) {
+    if (previousNode == nullptr) {
+        // Should be inserted at the beginning
+        if (head != nullptr) {
+            // There are more nodes
+            newNode->next = head->next;
+            newNode->next->previous = newNode;
+        }
+        head = newNode;
+        newNode->previous = nullptr;
+    } else if (previousNode->next == nullptr) {
+        // Should be inserted at the end
+        previousNode->next = newNode;
+        newNode->previous = previousNode;
+        newNode->next = nullptr;
+    }
 }
 
 template <class T>
-void DoublyLinkedList<T>::remove(Node<T>* deleteNode){
-	if (deleteNode->previous != nullptr){
-		// Is not the first node
-		deleteNode->previous->next = deleteNode->next;
-	}
-	if (deleteNode->next != nullptr){
-		// Is not the last node
-		deleteNode->next->previous = deleteNode->previous;
-	}
+void DoublyLinkedList<T>::remove(Node<T>* deleteNode) {
+    if (deleteNode->previous != nullptr) {
+        // Is not the first node
+        deleteNode->previous->next = deleteNode->next;
+    } else {
+        // Is the first node
+        head = deleteNode->next;
+        if (deleteNode->next != nullptr) {
+            deleteNode->next->previous = nullptr;
+        }
+    }
+    if (deleteNode->next != nullptr) {
+        // Is not the last node
+        deleteNode->next->previous = deleteNode->previous;
+    }else {
+        // Is the last node
+        if (deleteNode->previous != nullptr) {
+            deleteNode->previous->next = nullptr;
+        }
+    }
 }
